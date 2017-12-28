f1dd8b57cdfb65735161f28c033a72c998648cf1
A-Hussamadin <h.abushelha@gmail.com>
2017-12-28 10:56:00 +0300

    removing Roboot font

--- App.js
+++ App.js
@@ -106,12 +106,6 @@ function getCurrentRouteName(navigationState) {
 }
 export default class App extends React.Component {
 	state = { currentScreen: null }
-	async componentWillMount() {
-		await Expo.Font.loadAsync({
-			'Roboto': require('native-base/Fonts/Roboto.ttf'),
-			'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
-		});
-	}
 
 	componentDidMount() {
 		setLocalNotification();
